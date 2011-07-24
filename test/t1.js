abba9d6c687121758374351c93846eea311ca86c
Theo Schlossnagle <jesus@omniti.com>
2011-07-23 23:29:14 -0400

    more useful test primitives

--- test/t1.js
+++ test/t1.js
@@ -2,8 +2,12 @@ var Test = require('trial').Test,
     Trial = require('trial').Trial;
 
 var t1 = new Test(function() {
-  this.succeed("success test");
-}, { name: 'Hello', plan: 1 });
+  this.ok(true, "ok test");
+  this.is(1+2, 3, "is test");
+  this.isnt(2+2, 3, "isn't test");
+  this.like("food", /o{2}/, "like test");
+  this.unlike("fewd", /o{2}/, "unlike test");
+}, { name: 'Hello', plan: 5 });
 
 var trial = new Trial();
 trial.add(t1);
