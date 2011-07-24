482e68a0551a62ba9878a0449c1a005c63dcb67d
Theo Schlossnagle <jesus@omniti.com>
2011-07-23 23:11:38 -0400

    test skip

---
+++ test/t7.js
@@ -0,0 +1,10 @@
+var Test = require('trial').Test,
+    Trial = require('trial').Trial;
+
+var t1 = new Test(function() {
+  this.skip("skip test");
+}, { name: 'Skippy', plan: 1 });
+
+var trial = new Trial();
+trial.add(t1);
+trial.run();
