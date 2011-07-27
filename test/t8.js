8e869f927d5e6cdab3a3db6e7e2da7d32001d1d6
Theo Schlossnagle <jesus@omniti.com>
2011-07-27 17:50:50 -0400

    is_deeply

---
+++ test/t8.js
@@ -0,0 +1,17 @@
+var Test = require('trial').Test,
+    Trial = require('trial').Trial;
+
+var input = {
+  a: { b: [1,2,3,4,"hahaha"], c: true },
+  d: 1,
+  e: null,
+  f: "fruit"
+};
+
+var t1 = new Test(function() {
+  this.is_deeply(input, input, "deeply test");
+}, { name: 'Hello', plan: 1 });
+
+var trial = new Trial({ verbose: process.env.VERBOSE });
+trial.add(t1);
+trial.run();
---
+++ test/t9.js
@@ -0,0 +1,27 @@
+var Test = require('trial').Test,
+    Trial = require('trial').Trial;
+
+var input = {
+  a: { b: [1,{ t: [1,2,3,4,5,{s:true}]
+             },
+           3,4,"hahaha"], c: true },
+  d: 1,
+  e: null,
+  f: "fruit"
+}, output = {
+  a: { b: [1,{ t: [1,2,3,4,5,{s:false}]
+             },
+           3,4,"hahaha"], c: true },
+  d: 1,
+  e: null,
+  f: "fruit"
+};
+
+
+var t1 = new Test(function() {
+  this.is_deeply(input, output, "deeply test");
+}, { name: 'Hello', plan: 1 });
+
+var trial = new Trial({ verbose: process.env.VERBOSE });
+trial.add(t1);
+trial.run();
